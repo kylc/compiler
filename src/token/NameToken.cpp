9b81a396947fe409d32c5a3acb1522a1c30f465a
Kyle Cesare <kcesare@gmail.com>
2014-01-31 21:43:41 -0800

    Remove debugging.

--- src/token/NameToken.cpp
+++ src/token/NameToken.cpp
@@ -8,7 +8,6 @@ boost::shared_ptr<Token> NameToken::parse(std::fstream &fs) {
     char next = fs.get();
 
     switch(state) {
-      std::cout << "In state: " << state << " got: " << next << std::endl;
       case 0:
         if((next >= 'A' && next <= 'Z') || (next >= 'a' && next <= 'z') || next == '_') {
           state = 1;
