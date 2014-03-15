dd00f5fce301b44985ae4695fbfb94ba5d966e8f
Kyle Cesare <kcesare@gmail.com>
2014-03-14 21:16:22 -0700

    Emit warning if variable assignment type is incorrect.

--- src/tree/AssignNode.cpp
+++ src/tree/AssignNode.cpp
@@ -1,5 +1,7 @@
 #include "tree/AssignNode.h"
 
+#include <boost/log/trivial.hpp>
+
 AssignNode::AssignNode(boost::shared_ptr<NameToken> name) : name(name) {
 }
 
@@ -9,6 +11,11 @@ void AssignNode::print() {
 
 std::string AssignNode::emitTree() {
   std::string varname = name->getText();
+  Variable *var = variables->lookup(varname);
+
+  if(children[0]->getType() != var->getType()) {
+    BOOST_LOG_TRIVIAL(warning) << "Assigning wrong type to variable";
+  }
 
   return children[0]->emitTree() + " TO " + varname + " ";
 }
