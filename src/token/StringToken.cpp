52ccdff24a1eaec2adb77b5a378a9f6949737d3d
Kyle Cesare <kcesare@gmail.com>
2014-03-14 21:02:13 -0700

    Add support for arbitrary string characters (not restricted to alphanum).

--- src/token/StringToken.cpp
+++ src/token/StringToken.cpp
@@ -1,30 +1,9 @@
 #include "token/StringToken.h"
 
-#include "util/StateMachineBuilder.h"
-
 boost::shared_ptr<Token> StringToken::parse(std::istream &is, SymbolTablePtr symbols) {
-  StateMachineBuilder b;
-  // b.addState(0);
-  b.addTransition('"', 0, 1);
-  b.addState(1);
-
-  for(char c = 'A'; c < 'z'; c++) {
-    b.addConsumingTransition(c, 1, 1);
-  }
-
-  b.addConsumingTransition(' ', 1, 1);
-  b.addTransition('"', 1, 2);
-  b.addEndState(2);
-
-  StateMachine m = b.build();
-  if(m.consumeFromStream(is) == StateMachine::Accept) {
-    return boost::shared_ptr<Token>(new StringToken(m.getConsumed()));
-  } else {
-    return NULL;
-  }
-
-  /*
   int state = 0;
+  std::string value = "";
+
   while(true) {
     char next = is.get();
 
@@ -39,16 +18,13 @@ boost::shared_ptr<Token> StringToken::parse(std::istream &is, SymbolTablePtr sym
       case 1:
         if(next == '"') {
           return boost::shared_ptr<Token>(new StringToken(value));
-        } else if(std::isalnum(next) || std::isspace(next)) {
-          value += next;
         } else {
-          state = REJECT_STATE;
+          value += next;
         }
         break;
       case REJECT_STATE:
         return NULL;
     }
   }
-  */
 }
 
